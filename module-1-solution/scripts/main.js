5f76acbdce1fe2e470e83efd4faee173d953a757
Viktor <viktor.shapoval97@gmail.com>
2017-07-27 22:40:16 +0400

    Implement handlign zero items case Add styles

--- module-1-solution/scripts/main.js
+++ module-1-solution/scripts/main.js
@@ -9,8 +9,20 @@
     $scope.lunchItemsString = '';
     $scope.lunchStatus = '';
 
+    $scope.controllerClass = '';
+
+    var lunchListOfStatuses = {'tooMuch': 'Too much!', 'empty': 'Please enter data first', 'success': 'Enjoy!'};
+
     $scope.displayLunchStatus = function () {
-      $scope.lunchStatus = checkLunch($scope.lunchItemsString);
+      var status = checkLunch($scope.lunchItemsString);
+      var message = lunchListOfStatuses[status];
+      $scope.lunchStatus = message;
+
+      if ( status == 'empty' ) {
+        $scope.controllerClass = 'empty'
+      } else {
+        $scope.controllerClass = 'filled';
+      }
     };
 
     function checkLunch(string) {
@@ -20,10 +32,12 @@
         return word.length > 0;
       });
 
-      if ( lunchItems.length > 3) {
-        return 'Too much!';
+      if ( lunchItems.length > 3 ) {
+        return 'tooMuch';
+      } else if (lunchItems.length === 0){
+        return 'empty';
       } else {
-        return 'Enjoy!';
+        return 'success';
       }
     }
   }
